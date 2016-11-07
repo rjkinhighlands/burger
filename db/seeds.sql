5b6203ae5325ad8fe5f20a115779ce55105eb158
Robert J. Kelly <rjkelly@Roberts-MBP.home>
2016-11-07 18:34:13 -0500

    monday homework

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Venison Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Gator Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Kangaroo Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Emu Bacon Cheeseburger');
+
